408ca2087de01bad6161e18ef252119dbbfc1df4
keil <keil@keilovergames.com>
2019-06-18 11:08:06 -0700

    switch added

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-// app.all('*', function(req, res, next) {
-//   // res.header("Access-Control-Allow-Origin", "*");
-//   // res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   // next();
-// });
+app.use(cors({
+}));
 
 var localCity = '';
 var localState = '';
@@ -29,39 +25,23 @@ var data =
     sales: ''
 
 }
-// exports.helloWorld = (req, res) => {
-//     waApi.getFull({
-//         input: 'moving Redmond,Washington to New York,New York',
-//       }).then((queryresult) => {
-
-//         population = queryresult.pods[1].subpods[0].img.src;
-//         medianHouse = queryresult.pods[2].subpods[0].img.src;
-//         unemployment = queryresult.pods[3].subpods[0].img.src;
-//         crimeRate = queryresult.pods[4].subpods[0].img.src;
-//         salesTax = queryresult.pods[5].subpods[0].img.src;
-
-//         data.pop = population;
-//         data.medianH = medianHouse;
-//         data.unemployed = unemployment;
-//         data.crime = crimeRate;
-//         data.sales = salesTax;
-
-//         let datajson = JSON.stringify(data); 
-//         res.json(datajson);
-//         fs.writeFileSync('data.json', datajson);
-//       }).catch(console.error)
-//   };
 
   app.get('/',(req,res)=>{
     waApi.getFull({
         input: `moving  ${req.query.startCity} ${req.query.startState} to ${req.query.endCity} ${req.query.endState}`,
       }).then((queryresult) => {
         // res.send(queryresult);
-        population = queryresult.pods[1].subpods[0].img.src;
-        medianHouse = queryresult.pods[2].subpods[0].img.src;
-        unemployment = queryresult.pods[3].subpods[0].img.src;
-        crimeRate = queryresult.pods[4].subpods[0].img.src;
-        salesTax = queryresult.pods[5].subpods[0].img.src;
+
+        for (let i = 0; i < queryresult.pods.length; i++) {
+          
+          populate(queryresult.pods[i]);
+          
+        }
+        // population = queryresult.pods[2].subpods[0].img.src;
+        // medianHouse = queryresult.pods[3].subpods[0].img.src;
+        // unemployment = queryresult.pods[4].subpods[0].img.src;
+        // crimeRate = queryresult.pods[5].subpods[0].img.src;
+        // salesTax = queryresult.pods[6].subpods[0].img.src;
 
         data.pop = population;
         data.medianH = medianHouse;
@@ -69,9 +49,45 @@ var data =
         data.crime = crimeRate;
         data.sales = salesTax;
 
-        res.send(data)
+        res.send(data);
       }).catch(console.error)
   })
+
+  function populate(val)
+  {
+    switch(val.title)
+    {
+      case "Populations":
+          console.log("pop")
+          population = val.subpods[0].img.src;
+        break;
+      case "Housing sale prices":
+          console.log("housing")
+
+          medianHouse = val.subpods[0].img.src;
+        break;      
+      case "Unemployment rates":
+          console.log("employment")
+
+          unemployment = val.subpods[0].img.src;
+        break;      
+      case "Crime rates":
+          console.log("crime")
+
+          crimeRate = val.subpods[0].img.src;
+        break;
+      case "Sales taxes":
+          console.log("ST")
+
+          salesTax = val.subpods[0].img.src;
+        break;
+      default:
+          console.log("error");
+        break;
+
+    }
+
+  }
 
   app.listen(PORT);
 
